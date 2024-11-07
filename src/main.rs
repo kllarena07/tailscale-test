631602d4aa368e066043666c234a284f3a08778f
Kieran Llarena <baymax709@gmail.com>
2024-11-07 00:24:29 -0500

    init: index + secret route on actix web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, HttpResponse, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello, World!")
 }
+
+#[get("/secret")]
+async fn secret() -> impl Responder {
+    HttpResponse::Ok().body("This is a secret page.")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let addr = "127.0.0.1:3000";
+    println!("Listening on {}", addr);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(secret)
+    })
+    .bind(addr)?
+    .run()
+    .await
+}
+
